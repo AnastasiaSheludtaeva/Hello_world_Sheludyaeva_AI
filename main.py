2328a276739ae0f3008d69e0fef5a447aec84538
AnastasiaSheludtaeva <sheludyaeva.ai@edu.spbstu.ru>
2026-02-17 10:15:55 +0300

    Добавил файл main.ру

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-# This is a sample Python script.
+user_name = "Студент"
+number_a = 15
+number_b = 7
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(f"Привет, {user_name}!")
+print(f"Сумма чисел {number_a} и {number_b} равна {number_a + number_b}")
